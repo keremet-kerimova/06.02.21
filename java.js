6aa153b334dc33fd918d1e23e641af5bad55c277
Your Name <you@example.com>
2021-02-07 20:11:49 +0600

    added file java

--- java.js
+++ java.js
@@ -2,11 +2,12 @@ const setBg = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
     color.innerHTML = "#" + randomColor;
-  }
+  };
   
   genNew.addEventListener("click", setBg);
   setBg();
 
   function js_style () {
     document.getElementById ("table").text.style.fontSize = "14pt";  
-  }    
+  };
+  
