a5898b83b466e5059abc16d5cee4781413510462
Your Name <you@example.com>
2021-02-08 14:12:21 +0600

    changing code

--- java.js
+++ java.js
@@ -9,26 +9,3 @@ function generateRandomColor()
     //random color will be freshly served
 }
 
-
-// let example3 = document.querySelector("h1");
-//     h1.style.fontSize = "20px";
-
-
-// example4.addEventListener("click", function(){
-//     let example3 = document.querySelector("h1");
-//     h1.innerText++;
-// }
-
-
-// const setBg = () => {
-//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-//     document.body.style.backgroundColor = "#" + randomColor;
-//     color.innerHTML = "#" + randomColor;
-//   };
-  
-//   genNew.addEventListener("click", setBg);
-//   setBg();
-
-//   function js_style () {
-//     document.getElementById ("table").text.style.fontSize = "14pt";  
-//   };
